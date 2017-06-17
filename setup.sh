ce3701845fca9c0b8f4b586ae2082358595705e1
philippegorley <gorley.philippe@gmail.com>
2017-06-17 15:55:08 -0400

    removed debug nmessages

--- setup.sh
+++ setup.sh
@@ -6,11 +6,8 @@ file_list="bash_aliases bashrc gitconfig profile vimrc"
 
 mkdir -p $backup
 cd $dir
-echo "Moving existing dotfiles from ~ to $backup"
 for file in $file_list; do
     echo "Checking $file"
-    echo "Moving from ~/.$file to $backup/$file"
     mv -f "~/.$file" "$backup/$file"
-    echo "Creating symlink to $file"
     ln -f -s $dir/$file ~/.$file
 done

2d73d96f8f98ae13f48597116e5eb15036787cbb
philippegorley <gorley.philippe@gmail.com>
2017-06-17 15:54:20 -0400

    fixed typo

--- setup.sh
+++ setup.sh
@@ -4,11 +4,13 @@ dir="$HOME/dev/dotfiles"
 backup="$HOME/dev/dotfiles.bak"
 file_list="bash_aliases bashrc gitconfig profile vimrc"
 
-mkdir -p $olddir
+mkdir -p $backup
 cd $dir
 echo "Moving existing dotfiles from ~ to $backup"
 for file in $file_list; do
-    mv "~/.$file" "$backup/$file"
+    echo "Checking $file"
+    echo "Moving from ~/.$file to $backup/$file"
+    mv -f "~/.$file" "$backup/$file"
     echo "Creating symlink to $file"
-    ln -s $dir/$file ~/.$file
+    ln -f -s $dir/$file ~/.$file
 done

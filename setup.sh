fb33de220c0daf2de0c3ce1d822220f5a7ae2e91
philippegorley <gorley.philippe@gmail.com>
2017-06-17 16:11:49 -0400

    quickfix

--- setup.sh
+++ setup.sh
@@ -7,7 +7,10 @@ file_list="bash_aliases bashrc gitconfig profile vimrc"
 mkdir -p $backup
 cd $dir
 for file in $file_list; do
-    echo "Checking $file"
-    mv -f "~/.$file" "$backup/$file"
-    ln -f -s $dir/$file ~/.$file
+    if [[ -e "$HOME/.$file" ]]; then
+        echo "Back up $HOME/.$file to $backup/$file"
+        mv "$HOME/.$file" "$backup/$file"
+    fi
+    echo "Creating symlink to $file"
+    ln -s $dir/$file ~/.$file
 done
